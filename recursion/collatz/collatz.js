9b905175a38340aace608b61f679841f4fde70b3
jacoder <mantle@vivaldi.net>
2024-06-30 22:59:34 -0500

    COLLATZ
    complete the collatz program, accepting a user input and generating
    number of steps taken to get to 1.

--- recursion/collatz/collatz.js
+++ recursion/collatz/collatz.js
@@ -1,5 +1,35 @@
-const number = 100;
+/*
+    Recursive program that calculates how many steps it takes 
+    to get to 1 if you start from n and recurse as indicated in README_COLLATZ.
+    @datetime:: June 30, 2024 10:43 pm (UTC-5)
+    @author:: jac0der
+*/
+let number;
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter number: ', (answer) => {
+    number = answer;
+    console.log(collatz(number))
+
+    rl.close();
+});
+
+
+/**
+ * Function to calculate the number of steps taken
+   to get to 1, given a number n, using the algorithm
+   for the collatz sequence, as outlined in the 
+   README_COLLATZ read me file.
+ * @param {*} number -> value enetered by user
+ * @returns -> number of steps taking to get to 1, from 
+                a number n.
+ */
 function collatz(number)
 {
     step_count = 1;
@@ -19,5 +49,3 @@ function collatz(number)
 
     return step_count;
 }
-
-console.log(collatz(number))
