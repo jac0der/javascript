c0b6a558b31f262733d44bb317bebde2fed9152d
jacoder <mantle@vivaldi.net>
2024-11-28 02:26:15 -0500

    CONSOLE.LOG
    Added a html page with a simple script tag to see the log messages
    within the browsers dev tools.
    
    Used template literals to do string interpolation, passing in a value in
    a string.

--- writeoutput/consolelog.js
+++ writeoutput/consolelog.js
@@ -7,8 +7,39 @@ let competition = "Wimbledon Grandslam";
 console.log(competition);
 
 
-// Using Substitution Strings
+/*
+    In JavaScript, I can combine strings and variables in 
+    console.log() using the following methods:
+*/
+
+
+// 1: Using Substitution Strings
 let count = 5;
 console.log("There are %d items in your basket.", count);
 
 // Output: There are 5 items in your basket.
+// %d (substitution string) is a placeholder for a decimal or integer number.
+
+// 2: Using Template Literals
+// I enclose a message inside two backticks ` ` to utilize template literals.
+
+let amount = 5;
+
+// use template literals
+let message = `There are ${count} items in your basket.`;
+
+console.log(message);
+
+// Output: There are 5 items in your basket.
+
+
+
+
+
+
+
+
+
+
+
+
